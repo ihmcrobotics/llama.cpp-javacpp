ebeb866af71526a6249ed3fbf7285d168c8a7492
Duncan Calvert <dcalvert@ihmc.org>
2025-02-19 16:00:53 -0600

    Switch to 22.04

--- cppbuild.bash
+++ cppbuild.bash
@@ -9,11 +9,13 @@ LLAMACPP_VERSION=b4743
 # Non-shallow clone required for llama.cpp build
 git -c advice.detachedHead=false clone -b $LLAMACPP_VERSION https://github.com/ggml-org/llama.cpp.git llama.cpp-$LLAMACPP_VERSION
 
-cp ../patches/CMakeLists.txt.gguf-cuda.patch llama.cpp-$LLAMACPP_VERSION/CMakeLists.txt.gguf-cuda.patch
+# Ubuntu 20.04 ships with cmake 3.16.3, where llama.cpp ggml-cuda requires at least 3.18
+# This patch file currently not working yet. nvcc error during build
+# cp ../patches/CMakeLists.txt.gguf-cuda.patch llama.cpp-$LLAMACPP_VERSION/CMakeLists.txt.gguf-cuda.patch
 
 cd llama.cpp-$LLAMACPP_VERSION
 
-patch ggml/src/ggml-cuda/CMakeLists.txt CMakeLists.txt.gguf-cuda.patch
+# patch ggml/src/ggml-cuda/CMakeLists.txt CMakeLists.txt.gguf-cuda.patch
 
 cmake -B build -DCMAKE_LIBRARY_PATH=/usr/local/cuda/lib64/stubs -DGGML_CUDA=ON
 cmake --build build --config Release
