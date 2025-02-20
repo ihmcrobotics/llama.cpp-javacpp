066409b2de32d2cd8cec27600f9aa28c19d5e957
Duncan Calvert <dcalvert@ihmc.org>
2025-02-20 16:52:55 -0600

    Add more to LlamaCPPConfig.

--- src/main/java/us/ihmc/llamacpp/LlamaCPPConfig.java
+++ src/main/java/us/ihmc/llamacpp/LlamaCPPConfig.java
@@ -2,27 +2,41 @@ package us.ihmc.llamacpp;
 
 import org.bytedeco.javacpp.annotation.Platform;
 import org.bytedeco.javacpp.annotation.Properties;
+import org.bytedeco.javacpp.tools.Info;
 import org.bytedeco.javacpp.tools.InfoMap;
 import org.bytedeco.javacpp.tools.InfoMapper;
 
 @Properties(value = {
       @Platform(
-            include = {"llama.h"},
-            linkpath = "lib",
-            link = {"llama"},
-            preload = "jnillamacpp"
+            include = {"ggml.h", "ggml-backend.h", "ggml-cpu.h", "ggml-cuda.h", "gguf.h", "llama.h"},
+            link = {"llama", "ggml", "ggml-base", "ggml-cpu", "ggml-cuda"},
+            preload = "jnillamacpp",
+            define = {"GGML_USE_CUDA", "GGML_USE_NUMA"}
       ),
       @Platform(
             value = "linux",
             includepath = {"include", "/usr/local/cuda/include"}
       ),
 },
-      target = "us.ihmc.llama",
-      global = "us.ihmc.llama.global.llama"
+      target = "us.ihmc.llamacpp",
+      global = "us.ihmc.llamacpp.global.llamacpp"
 )
 public class LlamaCPPConfig implements InfoMapper {
    @Override
    public void map(InfoMap infoMap) {
+      // Skip problematic macros and attributes
+      infoMap.put(new Info("GGML_NORETURN").skip());
+      infoMap.put(new Info("GGML_RESTRICT").skip());
+      infoMap.put(new Info("GGML_BACKEND_API").skip());
 
+      // Map C++ typedefs to their corresponding Java classes
+      infoMap.put(new Info("ggml_backend_buffer_type_t").pointerTypes("ggml_backend_buffer_type"));
+      infoMap.put(new Info("ggml_backend_buffer_t").pointerTypes("ggml_backend_buffer"));
+      infoMap.put(new Info("ggml_backend_t").pointerTypes("ggml_backend"));
+      infoMap.put(new Info("ggml_backend_dev_t").pointerTypes("ggml_backend_device"));
+      infoMap.put(new Info("ggml_backend_event_t").pointerTypes("ggml_backend_event"));
+      infoMap.put(new Info("ggml_backend_reg_t").pointerTypes("ggml_backend_reg"));
+      infoMap.put(new Info("ggml_backend_sched_t").pointerTypes("ggml_backend_sched"));
+      infoMap.put(new Info("ggml_threadpool_t").pointerTypes("ggml_threadpool"));
    }
 }
