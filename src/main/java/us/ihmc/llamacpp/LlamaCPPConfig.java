fa305236cef7aa2e835f3d19911890a5d675f4e5
Duncan Calvert <dcalvert@ihmc.org>
2025-02-22 18:43:19 -0600

    Make LlamaCPPConfig easier to read.

--- src/main/java/us/ihmc/llamacpp/LlamaCPPConfig.java
+++ src/main/java/us/ihmc/llamacpp/LlamaCPPConfig.java
@@ -26,24 +26,24 @@ public class LlamaCPPConfig implements InfoMapper {
    @Override
    public void map(InfoMap infoMap) {
       // Skip problematic macros and attributes
-      infoMap.put(new Info("GGML_NORETURN").skip());
-      infoMap.put(new Info("GGML_RESTRICT").skip());
-      infoMap.put(new Info("GGML_BACKEND_API").skip());
+      infoMap.put(new Info().cppNames("GGML_NORETURN").skip());
+      infoMap.put(new Info().cppNames("GGML_RESTRICT").skip());
+      infoMap.put(new Info().cppNames("GGML_BACKEND_API").skip());
 
       // Map C++ typedefs to their corresponding Java classes
-      infoMap.put(new Info("ggml_backend_buffer_type_t").pointerTypes("ggml_backend_buffer_type"));
-      infoMap.put(new Info("ggml_backend_buffer_t").pointerTypes("ggml_backend_buffer"));
-      infoMap.put(new Info("ggml_backend_t").pointerTypes("ggml_backend"));
-      infoMap.put(new Info("ggml_backend_dev_t").pointerTypes("ggml_backend_device"));
-      infoMap.put(new Info("ggml_backend_event_t").pointerTypes("ggml_backend_event"));
-      infoMap.put(new Info("ggml_backend_reg_t").pointerTypes("ggml_backend_reg"));
-      infoMap.put(new Info("ggml_backend_sched_t").pointerTypes("ggml_backend_sched"));
-      infoMap.put(new Info("ggml_threadpool_t").pointerTypes("ggml_threadpool"));
-      infoMap.put(new Info("ggml_gallocr_t").pointerTypes("ggml_gallocr"));
-      infoMap.put(new Info("ggml_opt_context_t").pointerTypes("ggml_opt_context"));
-      infoMap.put(new Info("ggml_opt_dataset_t").pointerTypes("ggml_opt_dataset"));
-      infoMap.put(new Info("ggml_opt_result_t").pointerTypes("ggml_opt_result"));
+      infoMap.put(new Info().cppNames("ggml_backend_buffer_type_t").pointerTypes("ggml_backend_buffer_type"));
+      infoMap.put(new Info().cppNames("ggml_backend_buffer_t").pointerTypes("ggml_backend_buffer"));
+      infoMap.put(new Info().cppNames("ggml_backend_t").pointerTypes("ggml_backend"));
+      infoMap.put(new Info().cppNames("ggml_backend_dev_t").pointerTypes("ggml_backend_device"));
+      infoMap.put(new Info().cppNames("ggml_backend_event_t").pointerTypes("ggml_backend_event"));
+      infoMap.put(new Info().cppNames("ggml_backend_reg_t").pointerTypes("ggml_backend_reg"));
+      infoMap.put(new Info().cppNames("ggml_backend_sched_t").pointerTypes("ggml_backend_sched"));
+      infoMap.put(new Info().cppNames("ggml_threadpool_t").pointerTypes("ggml_threadpool"));
+      infoMap.put(new Info().cppNames("ggml_gallocr_t").pointerTypes("ggml_gallocr"));
+      infoMap.put(new Info().cppNames("ggml_opt_context_t").pointerTypes("ggml_opt_context"));
+      infoMap.put(new Info().cppNames("ggml_opt_dataset_t").pointerTypes("ggml_opt_dataset"));
+      infoMap.put(new Info().cppNames("ggml_opt_result_t").pointerTypes("ggml_opt_result"));
 
-      infoMap.put(new Info("struct ggml_backend_graph_copy").pointerTypes("ggml_backend_graph_copy"));
+      infoMap.put(new Info().cppNames("struct ggml_backend_graph_copy").pointerTypes("ggml_backend_graph_copy"));
    }
 }
